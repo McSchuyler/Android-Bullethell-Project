18d5c5594f8c9a42b81d64974dd71943d440e9cd
SchuylerChew <speedgolden@gmail.com>
2018-05-07 11:38:04 +0800

    Init
    Added
    -Sin wave script for projectile
    -Linear path for projectile

---
+++ Assets/Editor/LineInspector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LinePath))]
+public class LineInspector : Editor {
+
+    private void OnSceneGUI()
+    {
+        LinePath linePath = target as LinePath;
+        Transform handleTransform = linePath.transform;
+
+        //Transform local point to world space
+        Vector2 p1 = handleTransform.TransformPoint(linePath.p1);
+        Vector2 p2 = handleTransform.TransformPoint(linePath.p2);
+
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+        Handles.color = Color.white;
+        Handles.DrawLine(p1,p2);
+
+        //Enable handles
+        //Pass back world space coord to local point if changes happened to the point
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(linePath, "Move Point");
+            EditorUtility.SetDirty(linePath);
+            linePath.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+
+        EditorGUI.BeginChangeCheck();
+        p2 = Handles.DoPositionHandle(p2, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(linePath, "Move Point");
+            EditorUtility.SetDirty(linePath);
+            linePath.p2 = handleTransform.InverseTransformPoint(p2);
+        }
+    }
+}
---
+++ Assets/Script/LinePath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePath : MonoBehaviour {
+
+    public Vector2 p1, p2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
